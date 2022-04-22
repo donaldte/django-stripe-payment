476d9fe8f78fa12f821dc59d34a6048ca12f60e8
donald programmeur <81464575+donaldte@users.noreply.github.com>
2022-04-22 17:47:45 +0100

    Update models.py

--- Payment/products/models.py
+++ Payment/products/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import get_model_user
+User = get_model_user()
 
 # Create your models here.
 
 class Products(models.Model):
     name = models.CharField(_('name of product'), max_length=100)
     price = models.IntegerField(default=0) # in cents
+    description = models.TextField(default="description")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
 
     def __str__(self)-> str:
         return self.name
